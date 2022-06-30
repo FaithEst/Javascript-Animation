a5b9f7b6001cf79c4a549838fec00e125af27ecd
Faith Mutitu <mutitufaith53@gmail.com>
2022-06-30 08:21:15 +0300

    nested the p in new divs

--- DOM_manipulation/index.js
+++ DOM_manipulation/index.js
@@ -4,18 +4,50 @@ function renderAnimals(animal){
     animalsDisplay.className = "listItem"
     animalsDisplay.innerHTML = `
         <img src="${animal.imageUrl}">
-        <p class="animal-name>${animal.animalName}</p>
-        <p class="animal-desc">${animal.description}</p>
-        <p class="animal-donation">${animal.donations}</p>
+        <div class="content">
+            <p class="animal-name>${animal.animalName}</p>
+            <p class="animal-desc">${animal.description}</p>
+            <p class="animal-donation">$${animal.donations} donated</p>
+        </div>
     `
     console.log(animal.animalName);
     document.getElementById("animal-list").appendChild(animalsDisplay);
 }
 
-// fetch data function
+// fetch data function for all animals
 function fetchDataFromServer(){
     fetch('http://localhost:3000/animalData')
     .then(response => response.json())
     .then(resultData => resultData.forEach(animal => renderAnimals(animal)))
 }
-fetchDataFromServer();
+
+
+// event listener for a form 
+document.querySelector("form#animal-form").addEventListener('submit', handleSubmit);
+
+//event handlers
+function handleSubmit(e){
+    e.preventDefault();
+    let animalObj = {
+        name:e.target.name.value,
+        imageUrl:e.target.imageUrl.value,
+        description:e.target.description.value,
+        donations: 0
+    }
+    renderAnimals(animalObj);
+    adoptAnimal(animalObj);
+}
+
+
+// using POST request to update our json file
+function adoptAnimal(animalObj){
+    fetch('http://localhost:3000/animalData', {     // takes in 2 arguments
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify(animalObj)
+    })
+    .then(response => response.json())
+    .then(animalData => console.log(animalData))
+}
